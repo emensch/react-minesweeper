9c9175cd7800543f80c7e97b982101e87746c16e
Eli Mensch <eli.mensch@gmail.com>
2019-05-05 19:09:39 -0400

    Move relevant types to model

---
+++ src/store/models.ts
@@ -0,0 +1,18 @@
+export interface IBoardState {
+  [coord: string]: ITileState;
+}
+
+export interface ITileState {
+  adjacent: number | undefined;
+  flagged: boolean;
+  mined: boolean;
+}
+
+export enum GameStatus {
+  Ready,
+  Started,
+  Won,
+  Lost
+};
+
+export type XYCoord = { x: number, y: number };
