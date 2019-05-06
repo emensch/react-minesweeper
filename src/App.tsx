1594261692cd3defe6fddc179b818f826f41b2c8
Eli Mensch <eli.mensch@gmail.com>
2019-05-06 03:23:06 -0400

    Basic reset button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import './App.scss';
-import GameBoard from './components/GameBoard';
+import React from "react";
+import "./App.scss";
+import GameBoard from "./components/GameBoard";
+import store from "./store";
+import { resetGame } from "./store/actions";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <GameBoard />
+      <button className="game-button" onClick={() => store.dispatch(resetGame())}> Reset </button>
     </div>
   );
 }
